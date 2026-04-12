6d8dd3101114323c777097194f42f9d768ea70ee
Victor <victorcm759@gmail.com>
2026-04-12 15:12:08 +0200

    Nuevas tablas y correciones

--- sql/medallero.sql
+++ sql/medallero.sql
@@ -114,4 +114,5 @@ CREATE TABLE IF NOT EXISTS equipos_boccia (
 );
 
 INSERT INTO equipos_boccia (nombre, integrantes, provincia, comunidad) VALUES
-('Combinado BC4', 'Meritxell Pinto, Ramon Prat, Lucía Rovira', 'Cataluña');
+('Combinado BC4', 'Meritxell Pinto, Ramon Prat, Lucía Rovira', 'Cataluña'),
+('BC4s de Comkedem', 'Carlos Javier Vera, Víctor Català', 'Cataluña');
